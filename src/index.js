54a5b4fbf6d94eaad308ec2706e4d9bed2cd6424
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2020-10-20 16:50:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let number;
+if (n < 0){
+ number = n* (-1);
+   console.log(number);
+   number = String(number).split('').reverse();
+   if(number[0]==0){
+      number.shift();
+   }
+   return number.join('')
+}
+let result = String(n).split('');
+   return result.reverse().join('');
+
+
 }
